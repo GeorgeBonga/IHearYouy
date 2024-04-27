f0c8c951d74af99bbe177107e27ed1e493e63068
George Bonga <bongageorge18@gmail.com>
2024-04-27 11:48:04 +0300

    'changes'

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,7 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ["nativewind/babel"],
-    plugins: ["tailwindcss-react-native/babel"],
+    plugins: ["tailwindcss-react-native/babel","nativewind/babel"],
 
     
    
